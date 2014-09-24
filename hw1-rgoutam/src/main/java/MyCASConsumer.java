58f244e59bce051db421f88b9b16268783f6f1c4
Rahul Goutam <rahulgoutam07@gmail.com>
2014-09-24 00:36:33 -0400

    push

---
+++ hw1-rgoutam/src/main/java/MyCASConsumer.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.uima.UIMARuntimeException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceProcessException;
+
+
+public class MyCASConsumer extends CasConsumer_ImplBase {
+
+  BufferedWriter bout = null;
+  /**
+   * initialize bufferedwriter to write to outputfile
+   */
+  @Override
+  public void initialize() {
+    // TODO Auto-generated method stub
+    String name = (String)getConfigParameterValue("OutputPath");
+    File file = new File(name);
+    try {
+      bout = new BufferedWriter(new FileWriter(file));
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      throw new UIMARuntimeException(e);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      throw new UIMARuntimeException(e);
+    }
+  }
+  
+  /**
+   * @param arg0 CAS object
+   * write output to file using bufferedwriter 
+   */
+  @Override
+  public void processCas(CAS arg0) throws ResourceProcessException {
+    JCas jcas = null;
+    try {
+      jcas = arg0.getJCas();
+    } catch (CASException e) {
+      throw new UIMARuntimeException(e);
+    }
+    try {
+      FSIterator iter = jcas.getJFSIndexRepository().getAllIndexedFS(SentenceAnnotation.type);
+      String sentId = ((SentenceAnnotation) iter.next()).getSentID();
+      
+      iter = jcas.getJFSIndexRepository().getAllIndexedFS(NEAnnotation.type);
+      while(iter.hasNext()) {
+        NEAnnotation ne = (NEAnnotation)iter.next();
+        bout.write(sentId + "|" + ne.getBegin() + " " + ne.getEnd() + "|" + ne.getNamedEntity() + "\n");
+      }
+      bout.flush();
+    } catch(Exception e) {
+      throw new UIMARuntimeException(e);
+    }
+  }
+}
